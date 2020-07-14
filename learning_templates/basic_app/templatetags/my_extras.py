840375879c47aa664f49efcbc6f90c58c4488d1c
Neill <engelbrecht.neill979@gmail.com>
2020-07-14 17:21:13 +0200

    edited my_exstras.py

--- learning_templates/basic_app/templatetags/my_extras.py
+++ learning_templates/basic_app/templatetags/my_extras.py
@@ -1,12 +1,1 @@
-from django import template
 
-register = template.library()
-
-@register.filter(name="cut")
-def cut(value, arg):
-    """
-    This cuts out all values of "arg" from the string
-    """
-    return value.replace(arg, "")
-
-# register.filter("cut", cut)
